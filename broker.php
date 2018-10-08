5aa3a1c7463a706580378923a1825f98d1368792
Emile Caitan <35581078+Spartan719@users.noreply.github.com>
2018-10-08 15:23:31 -0400

    Add files via upload

--- broker.php
+++ broker.php
@@ -48,9 +48,9 @@ function doRegister($username, $password)
    $num = mysqli_num_rows($t);
    $file=__FILE_.PHP_EOL;
    $pathArray = explode("/",$file);
-   if ($num = 0) {
+   if ($num == 0) {
       echo "Registrated";
-      $s2 = "INSERT INTO users (username, passhash) VALUES ($username, $passhash)";
+      $s2 = "INSERT INTO users (username, passhash) VALUES ('$username', '$pass_hash')";
       $t2 = mysqli_query($db, $s2) or die (mysqli_error($db));
       return true;
    }
