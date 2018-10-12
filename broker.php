114bbdef383d5843b972b2425ae938b84df7738b
Emile Caitan <35581078+Spartan719@users.noreply.github.com>
2018-10-12 19:41:45 -0400

    Add files via upload

--- broker.php
+++ broker.php
@@ -16,9 +16,9 @@ function doLogin($username,$password)
    }
    $db = mysqli_connect('localhost', 'emile', 'Password7!', 'authtest');
    $pass_hash = hash('sha512', $password);
-   $s = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'",
-            mysqli_real_escape_string($username),
-            mysqli_real_escape_string($pass_hash));
+   $s = sprintf("SELECT * FROM users WHERE username='%s' AND passhash='%s'",
+            mysqli_real_escape_string($db, $username),
+            mysqli_real_escape_string($db, $pass_hash));
    $t = mysqli_query($db, $s) or die (mysqli_error($db));
    $num = mysqli_num_rows($t);
    $file=__FILE_.PHP_EOL;
@@ -46,17 +46,17 @@ function doRegister($username, $password)
 
    $db = mysqli_connect('localhost', 'emile', 'Password7!', 'authtest');
    $pass_hash = hash('sha512', $password);
-   $s = sprintf("SELECT * FROM users WHERE user='%s' AND password='%s'",
-            	mysqli_real_escape_string($username),
-           	mysqli_real_escape_string($pass_hash));
+   $s = sprintf("SELECT * FROM users WHERE username='%s' AND passhash='%s'",
+            	mysqli_real_escape_string($db, $username),
+           	mysqli_real_escape_string($db, $pass_hash));
    $t = mysqli_query($db, $s) or die (mysqli_error($db));
    $num = mysqli_num_rows($t);
    $file=__FILE_.PHP_EOL;
    $pathArray = explode("/",$file);
    if ($num == 0) {
       $s2 = sprintf("INSERT INTO users (username, passhash) VALUES ('%s', '%s')",
-		mysqli_real_escape_string($username),
-		mysqli_real_escape_string($pass_hash));
+		mysqli_real_escape_string($db, $username),
+		mysqli_real_escape_string($db, $pass_hash));
       $t2 = mysqli_query($db, $s2) or die (mysqli_error($db));
       echo "register";
       return true;
