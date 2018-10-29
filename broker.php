f3107012efcc4286e443ffa200aed24d7ee1e2fe
Emile Caitan <eac35@njit.edu>
2018-10-29 16:53:40 -0400

    bug fixes

--- broker.php
+++ broker.php
@@ -20,7 +20,7 @@ function doLogin($username, $password)
     $s         = sprintf("SELECT * FROM users WHERE username='%s' AND passhash='%s'", mysqli_real_escape_string($db, $username), mysqli_real_escape_string($db, $pass_hash));
     $t = mysqli_query($db, $s) or die(mysqli_error($db));
     $num       = mysqli_num_rows($t);
-    $file      = __FILE_ . PHP_EOL;
+    $file      = __FILE__ . PHP_EOL;
     $pathArray = explode("/", $file);
     if ($num > 0) {
         echo "success";
@@ -37,9 +37,8 @@ function doLogin($username, $password)
     $handle = fopen($file, 'a') or die('Cannot open file: ' . $file);
     echo fwrite($handle, $data);
     fclose($handle);
-  
-	echo log_api_data(1, 'test', '1990-10-10', true);
-
+    echo log_api_data(rand(0,1000), 'test', '1990-10-10', true);
+    echo "checkpoint";
     return $result;
     
     //$login = new loginDB();
@@ -102,7 +101,7 @@ function requestProcessor($request)
         case "validate_session":
             return doValidate($request['sessionId']);
     }
-    log_message($request);
+    //log_message($request);
     return array(
         "returnCode" => '0',
         'message' => "Server received request and processed"
